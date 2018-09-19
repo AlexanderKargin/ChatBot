fd009eb490060271811cfd938c8e2ad1fe7a1e48
Raccoon <Lyuda-mirosh@mail.ru>
2018-09-19 10:07:41 +0500

    asdf

--- src/com/company/Chat.java
+++ src/com/company/Chat.java
@@ -7,6 +7,7 @@ import java.util.Scanner;
 
 public class Chat {
     public void Run(){
+        
         Scanner input = new Scanner(System.in);
         System.out.println("Введите количество цифр в числе(4 или 5)");
         int numberOfDigits = input.nextInt();
