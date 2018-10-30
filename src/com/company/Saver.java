891c6e95fd3c02c55231ddf2bdef2cdc675d49d8
Raccoon_kek <Lyuda-mirosh@mail.ru>
2018-10-30 23:46:16 +0500

    Fixed High Score Saver

--- src/com/company/Saver.java
+++ src/com/company/Saver.java
@@ -6,8 +6,30 @@ import java.util.*;
 
 public class Saver {
     public void saveHighScore(User user) {
+        String fileName = "src/com/company/HighScore.txt";
+        String[] parts = getExistingHighScores(fileName);
+        if (parts.length == 15 && Integer.parseInt(parts[parts.length - 1]) <= (user.getTries())) {
+            System.out.println("You didn't set new high score :(");
+            return;
+        }
+        try (FileWriter writer = new FileWriter(fileName, false)) {
+            if (parts.length == 1){
+                writer.write("1/" + user.toString());
+                return;
+            }
+            ArrayList<Pair<String, Integer>> scores = getNewHighScores(parts, user);
+            for (var i = 0; i < scores.size(); i++){
+                writer.write(String.format("%d/%s/%d/\n", i + 1, scores.get(i).getKey(), scores.get(i).getValue()));
+            }
+        }
+        catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    private String[] getExistingHighScores(String fileName){
         StringBuilder text = new StringBuilder();
-        try (FileReader reader = new FileReader("src/com/company/history.txt")){
+        try (FileReader reader = new FileReader(fileName)){
             Scanner scan = new Scanner(reader);
             while(scan.hasNextLine()){
                 text.append(scan.nextLine());
@@ -17,45 +39,19 @@ public class Saver {
             System.out.println(e.getMessage());
         }
 
-        String[] parts = text.toString().split("/");
-        if (parts.length == 15 && Integer.parseInt(parts[parts.length - 1]) <= (user.getTries())) {
-            System.out.println("You didn't set new high score :(");
-            return;
-        }
+        return text.toString().split("/");
+    }
 
+    private ArrayList<Pair<String, Integer>> getNewHighScores(String[] parts, User user){
         ArrayList<Pair<String, Integer>> scores = new ArrayList<>();
-        try (FileWriter writer = new FileWriter("src/com/company/history.txt", false)) {
-            if (parts.length == 1){
-                writer.write("1/" + user.toString());
-                return;
-            }
-            else {
-                for (var i = 0; i < parts.length / 3; i++){
-                    scores.add(new Pair<>(parts[i * 3 + 1], Integer.parseInt(parts[i * 3 + 2])));
-                }
-            }
-            if (scores.size() == 5){
-                scores.remove(scores.get(4));
-            }
-            scores.add(new Pair<>(user.name, user.getTries()));
-            Collections.sort(scores, Comparator.comparing(p -> p.getValue()));
-
-            for (var i = 0; i < scores.size(); i++){
-                writer.write(String.format("%d/%s/%d/\n", i + 1, scores.get(i).getKey(), scores.get(i).getValue()));
-            }
+        for (var i = 0; i < parts.length / 3; i++){
+            scores.add(new Pair<>(parts[i * 3 + 1], Integer.parseInt(parts[i * 3 + 2])));
         }
-        catch (IOException e) {
-            System.out.println(e.getMessage());
+        if (scores.size() == 5){
+            scores.remove(scores.get(4));
         }
-
-//        try (FileReader reader = new FileReader("src/com/company/history.txt")){
-//            Scanner scan = new Scanner(reader);
-//            while(scan.hasNextLine()){
-//                System.out.println(scan.nextLine());
-//            }
-//        }
-//        catch (IOException e){
-//            System.out.println(e.getMessage());
-//        }
+        scores.add(new Pair<>(user.name, user.getTries()));
+        Collections.sort(scores, Comparator.comparing(p -> p.getValue()));
+        return scores;
     }
 }
